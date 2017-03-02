6878c0939f6b769ab98dbcdd66ab5305f41429cb
Luis Medinelli <luis.medinelli@gmail.com>
2017-03-02 00:40:56 -0300

    Fist Look on SoundViewController

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Luis Medinelli on 3/2/17.
+//  Copyright © 2017 iBeacon Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    @IBOutlet weak var soundTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
