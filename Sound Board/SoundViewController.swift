545a0b4b96cc600fbfb990d0af19b7b7d268773c
Luis Medinelli <luis.medinelli@gmail.com>
2017-03-04 02:54:50 -0300

    Final Version of App

--- Sound Board/SoundViewController.swift
+++ Sound Board/SoundViewController.swift
@@ -7,31 +7,113 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
     
+    var audioRecorder : AVAudioRecorder?  = nil
+    var audioPlayer : AVAudioPlayer? = nil
+    var audioUrl : URL?
     
     @IBOutlet weak var soundTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
+        
     }
     
+    
+    func setUpRecorder(){
+        do{
+            // create audio session
+            
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            // create url for audio file
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath,"audio.m4a"]
+            audioUrl = NSURL.fileURL(withPathComponents: pathComponents)!
+            //print(audioURL)
+            
+            // create setting to audio recording
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject
+            settings[AVSampleRateKey] = 44100.0 as AnyObject
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject
+            
+            // create audiorecorder object
+            audioRecorder = try AVAudioRecorder(url: audioUrl!, settings: settings)
+            audioRecorder?.prepareToRecord()
+        }catch let error as NSError {
+            
+            print(error)
+            
+        }
+        
+    }
     
     @IBAction func recordTapped(_ sender: Any) {
+        
+        //print("Record button tapped")
+        
+        if audioRecorder!.isRecording{
+            //print("Stoping recording")
+            // stop recording
+            audioRecorder!.stop()
+            
+            // change button to Record
+            recordButton.setTitle("Record", for: .normal)
+            playButton.isEnabled=true
+            addButton.isEnabled=true
+
+            
+        }else{
+            //print("Starting recording")
+            // stsrt recording
+            audioRecorder!.record()
+            
+            // chage button title to stop
+            recordButton.setTitle("Stop Recording", for: .normal)
+            
+        }
     }
     
-    
+
     
     @IBAction func playTapped(_ sender: Any) {
+        do{
+            try audioPlayer = AVAudioPlayer(contentsOf: audioUrl!)
+            audioPlayer!.play()
+        }catch let error as NSError{
+            print(error)
+        }
     }
     
     
     @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        let sound = Sound(context: context)
+        
+        sound.name = soundTextField.text!
+        sound.audio = NSData(contentsOf: audioUrl!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
     }
     
 
--- Sound Board/ViewController.swift
+++ Sound Board/ViewController.swift
@@ -7,12 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sounds : [Sound] = []
+    var audioPlayer : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            try sounds = context.fetch(Sound.fetchRequest())
+            tableView.reloadData()
+        }catch{
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        let sound = sounds[indexPath.row]
+        
+        cell.textLabel?.text = sound.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sound = sounds[indexPath.row]
+        
+        do{
+            
+            try audioPlayer = AVAudioPlayer(data: sound.audio! as Data)
+            audioPlayer!.play()
+        }catch let error as NSError{
+            print(error)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            //print("Hello Delete")
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(sounds[indexPath.row])
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+                try sounds = context.fetch(Sound.fetchRequest())
+                tableView.reloadData()
+            }catch{
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
